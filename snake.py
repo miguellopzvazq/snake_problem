4575887318d2d277ab42883d370a9600782f26fe
miguellopzvazq <miguellopezvazq@gmail.com>
2021-09-02 13:13:55 +0200

    problem variable snake

--- snake.py
+++ snake.py
@@ -1,16 +1,14 @@
 from itertools import product
 import numpy as np
 
-snake_OG = [[0,2], [0,1], [0,0], [1,0], [1,1], [1,2]]
-
-
 
 def move_snake(snake,direccion):
 
-   
     snake =  [[2,2], [3,2], [3,1], [3,0], [2,0], [1,0], [0,0]]
-    
-    
+    snake =  [[0,2], [0,1], [0,0], [1,0], [1,1], [1,2]]
+    snake =  [[5,5], [5,4], [4,4], [4,5]]
+ 
+
     for x in direccion:
         if(x == 'L'):
             move = np.add(snake[0], [-1,0])
@@ -31,25 +29,30 @@ def arreq_in_list(myarr, list_arrays):
     return next((True for elem in list_arrays if np.array_equal(elem, myarr)), False)
 
 def is_valid(move, board, snake):    
+   
     if(any(x<0 for x in move)):
         return False
+   
     elif(any(x<1 for x in np.subtract(board,move))):
         return False
-    #elif(np.any(np.all(move in snake[:-1]))):
+    
     elif(arreq_in_list(move, snake[:-1])):   
         return False 
+    
     else:
         return True
+     
+def valid_routes(board,snake, depth , tested_path = '', routes = []):
+
+    # print(snake)
     
-def valid_moves1(board,snake, depth ,res='',paths = []):
+    tested_routes = routes
+    new_path = ''
 
-    a= paths
-    path =''
-
-    if (len(res) == depth):
-        new_paths = paths + [res]
+    if (len(tested_path) == depth):
+        new_routes = routes + [tested_path]
         
-        return (new_paths)
+        return (new_routes)
         
  
         
@@ -60,58 +63,61 @@ def valid_moves1(board,snake, depth ,res='',paths = []):
     D = np.add(snake[0], [0,1])       
     
     
-    if(is_valid(L,board,snake) and res+'L' not in a):
+    if(is_valid(L,board,snake)):
 
-        path = res + 'L' 
-        snakeN = move_snake(snake_OG, path)
-        a = valid_moves1(board,snakeN, depth, path,a)
+        new_path = tested_path + 'L' 
+        new_snake = move_snake(snake, new_path)
+        
+        tested_routes = valid_routes(board, new_snake, depth, new_path, tested_routes)
 
        
-    if(is_valid(R,board,snake) and res+'R' not in a):
+    if(is_valid(R,board,snake)):
 
-        path = res + 'R'
-        snakeN = move_snake(snake_OG, path)
-
-        a = valid_moves1(board,snakeN, depth, path,a)
+        new_path = tested_path + 'R'
+        new_snake = move_snake(snake, new_path)
+        
+        tested_routes = valid_routes(board, new_snake, depth, new_path, tested_routes)
         
     
-    if(is_valid(U,board,snake) and res+'U' not in a):
+    if(is_valid(U,board,snake)):
 
-        path = res + 'U'
-        snakeN = move_snake(snake_OG, path)
-        a = valid_moves1(board,snakeN, depth, path,a)
+        new_path = tested_path + 'U'
+        new_snake = move_snake(snake, new_path)
+        
+        tested_routes = valid_routes(board, new_snake, depth, new_path, tested_routes)
     
     
-    if(is_valid(D,board,snake) and res+'D' not in a): 
+    if(is_valid(D,board,snake) ):
 
-        path = res + 'D'
-        snakeN = move_snake(snake_OG, path)
-
-        a = valid_moves1(board,snakeN, depth, path,a)
+        new_path = tested_path + 'D'
+        new_snake = move_snake(snake, new_path)
+        
+        tested_routes = valid_routes(board, new_snake, depth, new_path, tested_routes)
     
     
-    return(a)
+    return(tested_routes)
 
+def count_valid_routes(board, snake, depth):
+    return len(valid_routes(board, snake, depth))
+    
 
+# #Test 1:
+board =  [4, 3]
+snake =  [[2,2], [3,2], [3,1], [3,0], [2,0], [1,0], [0,0]]
+depth =  3
+print('\nTest1 - Expected result: 7      Tested result: '+ str(count_valid_routes(board, snake, depth)))
+
+    
 #Test 2:
 board =  [2, 3]
 snake =  [[0,2], [0,1], [0,0], [1,0], [1,1], [1,2]]
 depth =  10
-#Result: 1
+print('\nTest2 - Expected result: 1      Tested result: '+ str(count_valid_routes(board, snake, depth)))
 
 #Test 3:
 board =  [10, 10]
 snake =  [[5,5], [5,4], [4,4], [4,5]]
 depth =  4
-#Result: 81
+print('\nTest3 - Expected result: 81     Tested result: '+ str(count_valid_routes(board, snake, depth)))
 
-# #Test 1:
-# board =  [4, 3]
-# snake =  [[2,2], [3,2], [3,1], [3,0], [2,0], [1,0], [0,0]]
-# depth =  3
-# #Result: 7
 
-print(is_valid([-1,2],board,snake))
-print('\n'+ str(valid_moves1(board,snake, depth)))
-a = valid_moves1(board,snake, depth)
-#print(snake_paths(board,snake,depth))
